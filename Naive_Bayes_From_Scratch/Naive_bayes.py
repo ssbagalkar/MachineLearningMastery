0c94346bc057802abcff54047e257f0978155e5a
Saurabh S Bagalkar <sbagalka@asu.edu>
2017-11-18 11:47:01 -0600

    define function to split data into train and test set

--- Naive_Bayes_From_Scratch/Naive_bayes.py
+++ Naive_Bayes_From_Scratch/Naive_bayes.py
@@ -9,11 +9,30 @@
 #Tie it Together: Use all of the code elements to present a complete and standalone implementation of the Naive Bayes algorithm.
 
 import csv
+import random
 
 ## Write a function to load the csv data
 def loadCsv(filename):
-    lines = csv.reader(open(filename,"rb"))
-    dataset = list (lines)
-    for i in range(len(dataset)):
-        dataset[i] = [ float(x) for x in dataset[i]]
+    lines = csv.reader(open(filename,"r"))
+    dataset = list(lines)
     return dataset
+
+filename = 'pima-indians.csv'
+dataset = loadCsv(filename)
+#print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
+
+
+
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
+
+#dataset = [[1], [2], [3], [4], [5]]
+splitRatio = 0.67
+train, test = splitDataset(dataset, splitRatio)
+#print('Split {0} rows into train with {1} and test with {2}').format(len(dataset), train, test)
