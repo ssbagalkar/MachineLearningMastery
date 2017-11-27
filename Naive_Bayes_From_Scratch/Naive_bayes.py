48801e8f97652cf90013adce136f52ebc761330c
Saurabh S Bagalkar <sbagalka@asu.edu>
2017-11-27 15:49:58 +0530

    calculate probability using Gaussian pdf

--- Naive_Bayes_From_Scratch/Naive_bayes.py
+++ Naive_Bayes_From_Scratch/Naive_bayes.py
@@ -77,3 +77,8 @@ def summarizeByClass(dataset):
     return summaries
 
 summary = summarizeByClass(train)
+
+# calculate probability
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
