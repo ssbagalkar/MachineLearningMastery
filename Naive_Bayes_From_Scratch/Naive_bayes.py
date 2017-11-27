844f3e2ba5a064332b0405afb7e19a6d76aa81f9
Saurabh S Bagalkar <sbagalka@asu.edu>
2017-11-27 17:05:59 +0530

    Calculate probabilities and predict class labels

--- Naive_Bayes_From_Scratch/Naive_bayes.py
+++ Naive_Bayes_From_Scratch/Naive_bayes.py
@@ -82,3 +82,23 @@ summary = summarizeByClass(train)
 def calculateProbability(x, mean, stdev):
     exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
     return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+
+def calculateClassProbabilities(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
+
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
+
